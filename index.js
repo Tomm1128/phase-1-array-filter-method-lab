430285c9a20ae366777e82c6d2fae14290f8ba03
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-05-10 10:23:11 -0400

    Refactor fuzzyMatch with starstWith

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const findMatching = (drivers, name) => drivers.filter(driver => driver.toLowerCase() === name.toLowerCase())
 
-const fuzzyMatch = (drivers, name) => drivers.filter(driver => driver.substring(0, 1) === name.substring(0, 1))
+// const fuzzyMatch = (drivers, name) => drivers.filter(driver => driver.substring(0, 1) === name.substring(0, 1))
+
+const fuzzyMatch = (drivers, name) => drivers.filter(driver => driver.startsWith(name))
+
 
 const matchName = (drivers, name) => drivers.filter(driver => driver.name.toLowerCase() === name.toLowerCase())
